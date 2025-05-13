f3695bf44762cdad0ce1b6b7b644ad8b5e8c021a
Elia Giolli <you@example.com>
2025-05-13 18:56:06 +0200

    created another file dedicated to the routes and imported it into main.js

---
+++ src/data/router.js
@@ -0,0 +1,24 @@
+
+//import di librerie esterne
+import { createRouter, createWebHistory } from 'vue-router'
+
+// Importa le tue views (pagine)
+import Homepage from './views/Homepage.vue'
+import About from './views/About.vue'
+import HorrorFilms from './views/HorrorFilms.vue'
+import FilmList from './views/FilmList.vue'
+import Contacts from './views/Contacts.vue'
+
+// Crea il router
+const routes = [
+  { path: '/', component: Homepage },
+  { path: '/about', component: About },
+  { path: '/horror', component: HorrorFilms },
+  { path: '/films', component: FilmList },
+  { path: '/contacts', component: Contacts}
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
--- src/main.js
+++ src/main.js
@@ -2,29 +2,7 @@ import { createApp } from 'vue'
 import './style.css' // 
 import App from './App.vue'
 
-//import di librerie esterne
-import { createRouter, createWebHistory } from 'vue-router'
-
-// Importa le tue views (pagine)
-import Homepage from './views/Homepage.vue'
-import About from './views/About.vue'
-import HorrorFilms from './views/HorrorFilms.vue'
-import FilmList from './views/FilmList.vue'
-import Contacts from './views/Contacts.vue'
-
-// Crea il router
-const routes = [
-  { path: '/', component: Homepage },
-  { path: '/about', component: About },
-  { path: '/horror', component: HorrorFilms },
-  { path: '/films', component: FilmList },
-  { path: '/contacts', component: Contacts}
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+import { router } from './data/router'
 
 // Crea l'app Vue e usa il router
 const app = createApp(App)
