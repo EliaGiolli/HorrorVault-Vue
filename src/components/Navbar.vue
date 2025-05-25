4a306d9b34d7c955f88c6be621b3f6b2279d9371
Elia Giolli <you@example.com>
2025-05-25 11:36:56 +0200

    fixed responsive navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,35 @@
 <template>
      <nav class="nav-layout">
-        <ul>
-            <li><RouterLink to="/">Home</RouterLink></li>
-            <li><RouterLink to="/about">Su di noi</RouterLink></li>
-            <li><RouterLink to="/horror">Film horror</RouterLink></li>
-            <li><RouterLink to="/films">Lista film</RouterLink></li>
-            <li><RouterLink to="/contacts">Contattaci</RouterLink></li>
+        <ul class="nav-list">
+            <li class="nav-item"><RouterLink to="/">Home</RouterLink></li>
+            <li class="nav-item"><RouterLink to="/about">Su di noi</RouterLink></li>
+            <li class="nav-item"><RouterLink to="/horror">Film horror</RouterLink></li>
+            <li class="nav-item"><RouterLink to="/films">Lista film</RouterLink></li>
+            <li class="nav-item"><RouterLink to="/contacts">Contattaci</RouterLink></li>
+        </ul>
+        <!-- Toggle button -->
+        <Button @click="toggleNav" class="button-toggle">Click</Button>
+        <!-- Responsive Navbar  -->
+        <ul class="nav-list-responsive" v-if="isNavOpen">
+          <li class="nav-item"><RouterLink to="/">Home</RouterLink></li>
+          <li class="nav-item"><RouterLink to="/about">Su di noi</RouterLink></li>
+          <li class="nav-item"><RouterLink to="/horror">Film horror</RouterLink></li>
+          <li class="nav-item"><RouterLink to="/films">Lista film</RouterLink></li>
+          <li class="nav-item"><RouterLink to="/contacts">Contattaci</RouterLink></li>
         </ul>
     </nav>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
+import Button from './Button.vue';
+
+const isNavOpen = ref(false);
+
+const toggleNav = () =>{
+  isNavOpen.value = !isNavOpen.value;
+}
 </script>
 
 <style scoped>
@@ -23,37 +41,85 @@ import { RouterLink } from 'vue-router';
   align-items: center;
   padding: 1.5rem;
   border-bottom: 1px solid var(--color-light);
+  position: sticky;
+  top: 0;
+  right: 0;
+  
+}
 
-  ul {
-    width: 100%;
+.nav-list {
+  width: 100%;
+  display: none;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.nav-list-responsive {
+  background-color: var(--color-primary);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: right;
+  position: absolute;
+  top: 100px;
+  right: 0;
+  list-style: none;
+  padding: 1rem;
+  border-left: 5px solid var(--color-secondary);
+  width: 50%;
+  min-height: 100vh;
+  border-radius: 5px;
+  z-index: 100;
+}
+
+.nav-item a {
+  color: var(--color-text-primary);
+  text-decoration: none;
+  font-size: var(--font-md);
+  font-weight: bold;
+  transition: color 0.3s ease;
+  padding: 0.5rem;
+}
+
+.nav-item a:hover,
+.nav-item a:focus {
+  color: var(--color-accent);
+}
+
+/* router-link-active is managed by Vue Router */
+:global(.router-link-active) {
+  color: var(--color-text-active);
+  border-bottom: 2px solid var(--color-text-active);
+}
+
+.buttton-toggle{
+  background-color: var(--color-accent);
+  padding: 1rem;
+  color: var(--color-text-primary);
+  box-shadow: none;
+}
+.button-toggle:hover{
+  background-color: var(--color-text-accent-dark);
+  color: var(--color-text-primary);
+}
+
+@media (min-width: 760px) {
+  .nav-list-responsive {
+    display: none;
+  }
+  .nav-list {
     display: flex;
     justify-content: space-around;
     align-items: center;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-
-    li {
-      a {
-        color: var(--color-text-primary);
-        text-decoration: none;
-        font-size: var(--font-sm);
-        font-weight: bold;
-        transition: color 0.3s ease;
-        padding: 0.5rem;
-
-        &:is(:hover, :focus) {
-          color: var(--color-accent);
-        }
-      }
-    }
   }
 
-  /* router-link-active è gestita da Vue Router */
-  :global(.router-link-active) {
-    color: var(--color-text-active);
-    border-bottom: 2px solid var(--color-text-active);
+  .nav-item a{
+    font-size: var(--font-lg);
+  }
+  /* Hide the toggle button on large screens */
+  .button-toggle {
+    display: none;
   }
 }
-
 </style>
